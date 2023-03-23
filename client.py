c93e10d19438f0d25291e96e5e4c29e123c5f40c
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-03-23 20:45:12 +0530

    FileNotFound error resolved at client

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import os
 BUFFER_SIZE=4096
 HOST='127.0.0.1'
 PORT=8080
+SEPARATOR="<SEPARATOR>"
 
 clientsocket=socket.socket()
 clientsocket.connect((HOST,PORT))
@@ -12,12 +13,13 @@ print("Connected to server..")
 
 while 1:
     filename=input("Filename: ")
-    filesize=os.path.getsize(filename)
     if FileNotFoundError:
         print("File not found..")
         continue
+    filesize=os.path.getsize(filename)
+    break
 
-clientsocket.send(f"{filename} {filesize}".encode('utf-8'))
+clientsocket.send(f"{filename}{SEPARATOR}{filesize}".encode('utf-8'))
 progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
 with open(filename,"rb") as f:
     while 1:
