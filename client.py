3df2a8744004b360a3b7e481cd47386b59b0d6c9
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-03-26 23:23:13 +0530

    comments for description

--- client.py
+++ client.py
@@ -12,13 +12,14 @@ clientsocket.connect((HOST,PORT))
 print("Connected to server..")
 msg=input("Type 'get' to download files and 'post' to upload files..")
 clientsocket.send(msg.encode('utf-8'))
-if msg=="post":  
+
+if msg=="post":         # send file to server
     filename=input("File to upload: ")
     filesize=os.path.getsize(filename)
-    clientsocket.send(f"{filename}".encode('utf-8'))    # send file to server
+    clientsocket.send(f"{filename}".encode('utf-8'))    
     clientsocket.send(f"{filesize}".encode('utf-8'))
     progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename,"rb") as f:
+    with open(filename,"rb") as f:      # read data chunks from the file and send them to server
         while 1:
             bytes_read=f.read(BUFFER_SIZE)
             if not bytes_read:
@@ -27,15 +28,15 @@ if msg=="post":
             progress.update(len(bytes_read))
 
 elif msg=="get":    # to get a file from the server
-    filename=input("File to receive: ")
-    clientsocket.send(f"{filename}".encode('utf-8'))
-    filesize=int(clientsocket.recv(BUFFER_SIZE).decode('utf-8'))
-    if filesize==0:
+    filename=input("File to receive: ")    
+    clientsocket.send(f"{filename}".encode('utf-8'))         # give the filename to server
+    filesize=int(clientsocket.recv(BUFFER_SIZE).decode('utf-8'))    # receive the filesize of the file present in the server
+    if filesize==0:     # file not present in the server
         print("File not present..")
-    else:
+    else:       # file present in the server
         print("File present and retrieving..")
         progress=tqdm.tqdm(range(filesize), f"receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        with open (filename,"wb") as f:
+        with open (filename,"wb") as f:     # create a file and write the data received from the server.
             while 1:
                 bytes_read=clientsocket.recv(BUFFER_SIZE)
                 if not bytes_read:
--- server.py
+++ server.py
@@ -16,14 +16,14 @@ clientsocket,address=serversocket.accept()
 print(f"{address} connected..")
 
 
-msg=clientsocket.recv(BUFFER_SIZE).decode('utf-8')
-if msg=="post":
+msg=clientsocket.recv(BUFFER_SIZE).decode('utf-8')      # know whether client sends or receives a file
+if msg=="post":     # client wants to upload a file to the server
     filename=clientsocket.recv(BUFFER_SIZE).decode('utf-8')
-    filesize=clientsocket.recv(BUFFER_SIZE).decode('utf-8')     # receive file from the client.
+    filesize=clientsocket.recv(BUFFER_SIZE).decode('utf-8')    
     filename=os.path.basename(filename)
     filesize=int(filesize)
     progress=tqdm.tqdm(range(filesize), f"receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-    with open (filename,"wb") as f:
+    with open (filename,"wb") as f:     # open a file of the given name and write the received data into it (creation)
         while 1:
             bytes_read=clientsocket.recv(BUFFER_SIZE)
             if not bytes_read:
@@ -31,18 +31,18 @@ if msg=="post":
             f.write(bytes_read)
             progress.update(len(bytes_read))
 
-elif msg=="get":
-    filename=clientsocket.recv(BUFFER_SIZE).decode('utf-8')
+elif msg=="get":       # client wants a file from the server
+    filename=clientsocket.recv(BUFFER_SIZE).decode('utf-8')     # get the desired filename
     try:
         filesize=int(os.path.getsize(filename))
     except:
         filesize=0
-    if filesize==0:
+    if filesize==0:     # file not present in the server
         clientsocket.send(f"{filesize}".encode('utf-8'))
-    else:
+    else:       # file present in the server 
         clientsocket.send(f"{filesize}".encode('utf-8'))
         progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        with open(filename,"rb") as f:
+        with open(filename,"rb") as f:      # open fil ein read mode and send data chunks to the client
             while 1:
                 bytes_read=f.read(BUFFER_SIZE)
                 if not bytes_read:
