b3a511338c3a64388e47bbc342af8a12b44ee575
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-03-29 17:53:02 +0530

    ready to get files from the server

--- client.py
+++ client.py
@@ -13,33 +13,36 @@ clientsocket.send(command.encode())
 if command == "get":
     # get file name from user
     filename = input("Enter file name: ")
+    filename=filename.replace('\x00','')
     clientsocket.send(filename.encode())
 
     # receive file size from server
     filesize = int(clientsocket.recv(1024).decode())
     print(f"File size: {filesize} bytes")
 
-    progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit='B', unit_scale=True, unit_divisor=1024)
+    #progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit='B', unit_scale=True, unit_divisor=1024)
     # receive file data from server
     with open(filename, 'wb') as f:
         while filesize > 0:
             data = clientsocket.recv(1024)
             f.write(data)
             filesize -= len(data)
-            progress.update(data)
+            #progress.update(data)
 
     print(f"Received file: {filename}")
 
+''' 
 elif command == "post":
     # get file name from user
     filename = input("Enter file name: ")
-    clientsocket.send(filename.encode())
+    #filename=os.path.basename(filename)
+    clientsocket.send(f"{filename}".strip().encode('utf-8'))
 
     # send file size to server
-    filesize = os.path.getsize(filename)
-    clientsocket.send(str(filesize).encode())
+    filesize = int(os.path.getsize(filename))
+    #clientsocket.send(str(filesize).strip().encode())
 
-    progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit='B', unit_scale=True, unit_divisor=1024)
+    #progress=tqdm.tqdm(range(filesize), f"sending {filename}", unit='B', unit_scale=True, unit_divisor=1024)
     # send file data to server
     with open(filename, 'rb') as f:
         while True:
@@ -47,10 +50,10 @@ elif command == "post":
             if not data:
                 break
             clientsocket.send(data)
-            progress.update(data)
+            #progress.update(data)
 
     print(f"Sent file: {filename}")
 else:
     print("Unknown command")
-
+'''
 clientsocket.close()
